715df827278852a39e3a3a65ece517799c0d00fe
leduchoang0201 <leduchoang258@gmail.com>
2023-11-10 21:41:55 +0700

    update

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ function addSubject(){
             <td>Khu</td>
             <td>Ca</td>
             <td>Tên GV</td>
-            <td>Hành động</td>
+            <td>Chức năng</td>
         </tr>`;
 
     subject.forEach((subject, index) => {
@@ -44,12 +44,18 @@ function addSubject(){
                 <a href= '#' >Sửa</a> | <a href= '#' onclick='deleteSubject(${STTsubject})'>Xóa</a>
             </td>
         </tr>`
-    })
+    });
     document.getElementById('list-subject').innerHTML = tableContent;
         
         localStorage.setItem('subject', JSON.stringify(subject));
         this.saveSubject();
         updateSubjectTable();
+        document.getElementById("name").value = "";
+        document.getElementById("ID").value = "";
+        document.getElementById("class").value = "";
+        document.getElementById("building").value = "";
+        document.getElementById("lesson").value = "";
+        document.getElementById("nameGV").value = "";
     }
 }
 function saveSubject(){
@@ -78,7 +84,7 @@ function updateSubjectTable() {
         <td>Khu</td>
         <td>Ca</td>
         <td>Tên GV</td>
-        <td>Hành động</td>
+        <td>Chức năng</td>
     </tr>`;
 
     subject.forEach((subject, index) => {
@@ -93,13 +99,51 @@ function updateSubjectTable() {
             <td>${subject.lesson}</td>
             <td>${subject.nameGV}</td>
             <td>
-                <a href='#'>Sửa</a> | <a href='#' onclick='deleteSubject(${STTsubject})'>Xóa</a>
+                <a href='#' onclick='editSubject(${STTsubject})'>Sửa</a> | <a href='#' onclick='deleteSubject(${STTsubject})'>Xóa</a>
             </td>
         </tr>`;
     });
 
     document.getElementById('list-subject').innerHTML = tableContent;
 }
+function editSubject(STT){
+    let subject = localStorage.getItem('subject') ? JSON.parse(localStorage.getItem('subject')) : [];
+    if (subject.length > 0) {
+        document.getElementById('name').value = subject[STT].name;
+        document.getElementById('ID').value = subject[STT].ID;
+        document.getElementById('class').value = subject[STT].class;
+        document.getElementById('building').value = subject[STT].building;
+        document.getElementById('lesson').value = subject[STT].lesson;
+        document.getElementById('nameGV').value = subject[STT].nameGV;
+    }
+    
+}
+function button(STT){
+    let monhoc = document.getElementById('name').value;
+    let mamonhoc = document.getElementById('ID').value;
+    let phonghoc = document.getElementById('class').value;
+    let khu = document.getElementById('building').value;
+    let ca = document.getElementById('lesson').value;
+    let tenGV = document.getElementById('nameGV').value;
+    if (monhoc && mamonhoc && phonghoc && khu && ca && tenGV) {
+        let subject = localStorage.getItem('subject') ? JSON.parse(localStorage.getItem('subject')) : [];
+        
+        subject.push({
+            name: monhoc,
+            ID: mamonhoc,
+            class: phonghoc,
+            building: khu,
+            lesson: ca,
+            nameGV: tenGV
+        });
+        localStorage.setItem('subject', JSON.stringify(subject));
+        this.saveSubject();
+        updateSubjectTable();
+    }
+    let subject = localStorage.getItem('subject') ? JSON.parse(localStorage.getItem('subject')) : [];
+}
+
+
 function initPage() {
     updateSubjectTable();
     saveSubject();
